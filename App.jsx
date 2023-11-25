606628a1293924ad37dea8e8f491eb8349977e6f
Mastoora Turkmen <mastooraturkmen@gmail.com>
2023-11-25 08:12:04 +0430

    first commit

--- App.jsx
+++ App.jsx
@@ -12,27 +12,6 @@ export default function App() {
 		setPlayerName(prevName => prevName.trim() + " The Magnificent!")
 	}
 	
-/* Challenge
-
-	The hero of this RPG needs a name! Your task is to set up an input element as follows:
-	
-		1. The input should be in the space marked below and have the following characteristics: 
-	  	    - Class name: "name-input"	
-	  	    - Type: "text."
-	  	    - Maximum length: 16 characters.
-	  	    - Placeholder text: "Enter Your Character's Name".
-	  	    - Required: yes  
-	  	    - Disabled: yes if form has been submitted (see code on lines 7-11). 
-	  
-		2. The current value of playerName should be the current string the user has typed into the 
-		   input. Whenever the user's input changes, the state should be updated.
-		
-		3. The input should be controlled. Its value should be controlled by the playerName state.
-	
-		4. You should only write your code below. Nothing needs to be changed or added elsewhere!   
-		   If you complete these tasks correctly, you should receive a hero's name after you type 
-		   in your character's name and click "Continue"!
-*/
 	function handleChange(event) {
 		setPlayerName(event.target.value)
 	}
@@ -41,10 +20,7 @@ export default function App() {
 
 	return (
 		<form className="form" onSubmit={handleSubmit}>
-            <TopSection submitted={submitted} />
-            
-			{/*-----Your input below!----------*/}
-			
+            <TopSection submitted={submitted} />			
 			<input 
 				className="name-input"
 				type="text"
@@ -55,9 +31,6 @@ export default function App() {
 				onChange={handleChange}
 				value={playerName}
 			/>
-			
-			{/*-----Your input above!----------*/}
-            
             <BottomSection submitted={submitted} playerName={playerName} />
 		</form>
 	)
