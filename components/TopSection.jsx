b3795b8cd8d572aaaf7bdc6932e1b21d0c28e022
Mastoora Turkmen <mastooraturkmen@gmail.com>
2023-11-25 08:13:12 +0430

    first commit

--- components/TopSection.jsx
+++ components/TopSection.jsx
@@ -5,15 +5,15 @@ export default function TopSection({submitted}) {
     return (
         <div className="top-section">
         <h1 className={submitted && !classCondition ? "second-fade-in" : null}>
-			{!submitted || classCondition
-			    ? "The Legend of Scrimba"
-					: "A hero has risen! The hero's name is: "}
-			</h1>
-                    <div className="image-container">
-            			<img 
-                            className={!submitted || classCondition ? null : "fade-out-and-in" }
-                            src={!submitted || classCondition ? "./images/character1.png" : "./images/character2.png"} />
-                    </div>
+            {!submitted || classCondition
+                ? "The Legend of Scrimba"
+                    : "A hero has risen! The hero's name is: "}
+            </h1>
+                <div className="image-container">
+                    <img 
+                        className={!submitted || classCondition ? null : "fade-out-and-in" }
+                        src={!submitted || classCondition ? "./images/character1.png" : "./images/character2.png"} />
+                </div>
 
             </div>
     )
